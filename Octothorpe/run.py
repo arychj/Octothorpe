86e62ea4d060c23672f95ec136188c830e55e21e
Erik J. Olson <git@arych.com>
2016-11-02 19:13:51 -0500

    Missed run.py in previous commit

--- Octothorpe/run.py
+++ Octothorpe/run.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-import sys
+import sys, time
 
 from Octothorpe.Config import Config
 from Octothorpe.Database import Database
 from Octothorpe.Instruction import Instruction
 from Octothorpe.InstructionQueue import InstructionQueue
+from Octothorpe.Log import Log
 from Octothorpe.Manager import Manager
+
+from random import randint
 
 if len(sys.argv) != 2:
     print(f"\tUsage: {sys.argv[0]} config.xml")
@@ -17,11 +20,20 @@ else:
     manager = Manager()
     manager.Start()
 
+    for i in range(50):
+        payload = "{\"text\":\"" + ("*" * i) + "\"}"
+        instruction = Instruction(i, 1, time.time(), "Test", ("Echo" if randint(0,1) == 0 else "Test"), payload)
+        InstructionQueue.Enqueue(instruction)
+
     while(1):
-        line = input()
-        if(line == "stop"):
-            manager.Stop()
-            break
-        else:
-            pieces = line.split(";")
-            InstructionQueue.Push(Instruction(0, pieces[0], pieces[1]))
+        try:
+            line = input()
+            if(line == "stop"):
+                print("Stopping...")
+                manager.Stop()
+                break
+            else:
+                pieces = line.split(";")
+                InstructionQueue.Enqueue(Instruction(0, 1, time.time(), pieces[0], pieces[1]))
+        except Exception as e:
+            Log.Exception(e)
