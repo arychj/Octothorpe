64dd4fc3811361d8462711381f04ab4f698c9185
Erik J. Olson <git@arych.com>
2016-11-22 18:48:53 -0600

    Handle instructions with invalid methods

--- Octothorpe/Octothorpe/Service.py
+++ Octothorpe/Octothorpe/Service.py
@@ -12,7 +12,6 @@ from .Rule import Rule
 
 class Service(DynamicModule, metaclass=ABCMeta):
     _queue = None
-    _instruction = None
 
     @property
     def _module_type(self):
@@ -22,6 +21,10 @@ class Service(DynamicModule, metaclass=ABCMeta):
     def _emitted_event_types(self):
         return []
 
+    @property
+    def _event_type_default(self):
+        return f"_{self._instruction.Method.lower()}"
+
     def Describe(self, method_name):
         method = getattr(self, method_name, None)
         if(method == None):
@@ -30,7 +33,7 @@ class Service(DynamicModule, metaclass=ABCMeta):
             return list(inspect.signature(method).parameters)
 
     def Emit(self, event_type, payload):
-        if((event_type == self._instruction.Method) or (event_type in self._emitted_event_types)):
+        if((event_type == self._event_type_default) or (event_type in self._emitted_event_types)):
             Log.Debug(f"Event '{event_type}' emitted by {self._instruction.Service}.{self._instruction.Method}()")
 
             event = Event.Create(
@@ -66,11 +69,14 @@ class Service(DynamicModule, metaclass=ABCMeta):
         service._instruction = instruction
 
         method = getattr(service, instruction.Method, None)
-        if(instruction.Payload == None):
-            instruction.Result = method()
-        else:
-            instruction.Result = method(**instruction.Payload)
+        if(method != None):
+            if(instruction.Payload == None):
+                instruction.Result = method()
+            else:
+                instruction.Result = method(**instruction.Payload)
 
-        if(instruction.Result != None):
-            service.Emit(f"_{instruction.Method}.lower()", instruction.Result)
+            if(instruction.Result != None):
+                service.Emit(service._event_type_default, instruction.Result)
+        else:
+            Log.Error(f"Unknown method '{instruction.Method}' in '{instruction.Service}'")
 
