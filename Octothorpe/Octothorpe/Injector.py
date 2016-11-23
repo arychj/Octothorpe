18d787ad2f6157d02cc18214599ea0f0a7a522d9
Erik J. Olson <git@arych.com>
2016-11-22 18:50:43 -0600

    Make injectors daemons so blocked threads waiting for input will end
    Also handle invalid instructions

--- Octothorpe/Octothorpe/Injector.py
+++ Octothorpe/Octothorpe/Injector.py
@@ -24,35 +24,43 @@ class Injector(DynamicModule, metaclass=ABCMeta):
     def Stop(self):
         pass
 
-    def Call(self, service, method, payload):
-        instruction = Instruction.Create(
-            1,
-            service,
-            method,
-            payload
-        )
+    def Call(self, service=None, method=None, payload=None, instruction=None):
+        if(service and method and payload):
+            instruction = Instruction.Create(
+                1,
+                service,
+                method,
+                payload
+            )
 
-        InstructionQueue.Enqueue(instruction)
+        if(instruction):
+            InstructionQueue.Enqueue(instruction)
 
-        while(instruction.IsComplete == False):
-            time.sleep(0.1)
-        
-        return instruction.Result
+            while(instruction.IsComplete == False):
+                time.sleep(0.1)
+            
+            return instruction.Result
+        else:
+            Log.Error("Attempted to inject invalid instruction", tag=self._name)
 
     def Log(self, message):
         Log.System(message, tag=self._name)
             
+    def Error(self, message):
+        Log.Error(message, tag=self._name)
+    
     @staticmethod
     def StartAll():
         xInjectors = Config._raw(f"injectors/injector")
 
         for xInjector in xInjectors:
-            #module_name = (xInjector.attrib["module"] if ('module' in xInjector.attrib) else xInjector.attrib["name"])
             injector_type = Injector._get_module("injector", xInjector.attrib["name"])
             
             injector = injector_type()
             injector._name = xInjector.attrib['name']
+
             t = threading.Thread(target=injector.Start)
+            t.daemon = True
             t.start()
 
             Injector._active_injectors.append(injector)
@@ -61,6 +69,7 @@ class Injector(DynamicModule, metaclass=ABCMeta):
 
     @staticmethod
     def StopAll():
+        Log.System("Stopping injectors")
+
         for injector in Injector._active_injectors:
             injector.Stop()
-
