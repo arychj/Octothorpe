8e1c2b1d57650d12de5ee5d401d41a041189e40c
Erik J. Olson <git@arych.com>
2016-11-24 13:42:03 -0500

    Added basic SMS messaging service

---
+++ Octothorpe/Octothorpe/Services/Messaging.py
@@ -0,0 +1,58 @@
+import re
+from twilio.rest import TwilioRestClient
+
+from Octothorpe.Log import Log
+from Octothorpe.Service import Service
+
+class Messaging(Service):
+
+    def Send(self, protocol, to, message, subject = None):
+        result = None
+
+        protocol = protocol.lower()
+        if(protocol == "email"):
+            result = self._email(to, message, subject)
+        elif(protocol == "sms"):
+            result = self._sms(to, message)
+        elif(protocol == "phone"):
+            result = self._phone(to, message)
+        elif(protocol == "slack"):
+            result = self._slack(to, message)
+        else:
+            result = {"error": f"Unkown protocol '{protocol}'"}
+
+        if(result == None):
+            result = {"success": "Message sent"}
+
+        return result
+
+    def _email(self, to, message, subject):
+        pass
+    
+    def _sms(self, to, message):
+        client = TwilioRestClient(
+            self.GetString("sms/username"),
+            self.GetString("sms/password")
+        )
+
+        address = re.sub(self.GetString("sms/address_sanitize"), "", to)
+
+        if len(address) == 10:
+            address = '+1%s' % (address)
+        elif len(toaddress) == 11:
+            address = '+%s' % (address)
+        else:
+            self.Error(f"'{to}' is not a valid 10 or 11 digit number")
+            return
+
+        sms = client.sms.messages.create(
+            to = address,
+            from_ = self.GetString("sms/from"),
+            body = message
+        )
+    
+    def _phone(self, to, message):
+        pass
+
+    def _slack(self, to, message):
+        pass
