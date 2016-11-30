c5163d873b196587a14e5fb684d1cc7806fb6231
Erik J. Olson <git@arych.com>
2016-11-29 19:32:55 -0600

    Moved run logic into manager

--- Octothorpe/Octothorpe/Manager.py
+++ Octothorpe/Octothorpe/Manager.py
@@ -1,15 +1,41 @@
-import sys, threading
+import argparse, sys, threading
+from random import randint
 
+from .Config import Config
 from .Injector import Injector
+from .Instruction import Instruction
 from .TaskQueue import TaskQueue
 from .Log import Log
 from .Service import Service
-from .Event import Event
+from .Setup import Setup
 
 class Manager:
 
     @staticmethod
     def Start():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--config", dest ="config", required=True, help="Configuration File")
+        parser.add_argument("--test", dest ="test", type=int, default=None, help="Generate and queue n test instructions")
+        parser.add_argument("--setup", dest ="setup", action="store_true", help="Perform initial setup")
+
+        args = parser.parse_args()
+        Config.SetConfigFile(args.config)
+
+        if(args.setup):
+            Setup.Setup()
+
+        if(args.test != None):
+            for i in range(args.test):
+                payload = "{\"text\":\"" + ("*" * i) + "\"}"
+                instruction = Instruction.Create(
+                    1,
+                    "Test", 
+                    ("Echo" if randint(0,1) == 0 else "Test"), 
+                    payload
+                )
+
+                TaskQueue.Enqueue(instruction)
+
         TaskQueue.Start()
         Injector.StartAll()
 
@@ -19,11 +45,3 @@ class Manager:
 
         TaskQueue.Stop()
         Injector.StopAll()
-
-    @staticmethod
-    def Queue(instruction):
-        TaskQueue.Enqueue(instruction)
-
-    @staticmethod
-    def Process(instruction):
-        Service.Call(instruction)
--- Octothorpe/run.py
+++ Octothorpe/run.py
@@ -1,34 +1,4 @@
 #!/usr/bin/env python3
-import argparse, sys
-
-from Octothorpe.Config import Config
-from Octothorpe.Setup import Setup
-from Octothorpe.Instruction import Instruction
 from Octothorpe.Manager import Manager
-
-from random import randint
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--config", dest ="config", required=True, help="Configuration File")
-parser.add_argument("--test", dest ="test", type=int, default=None, help="Generate and queue n test instructions")
-parser.add_argument("--setup", dest ="setup", action="store_true", help="Perform initial setup")
-
-args = parser.parse_args()
-Config.SetConfigFile(args.config)
-
-if(args.setup):
-    Setup.Setup()
-
-if(args.test != None):
-    for i in range(args.test):
-        payload = "{\"text\":\"" + ("*" * i) + "\"}"
-        instruction = Instruction.Create(
-            1,
-            "Test", 
-            ("Echo" if randint(0,1) == 0 else "Test"), 
-            payload
-        )
-
-        Manager.Queue(instruction)
 
 Manager.Start()
